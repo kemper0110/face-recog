092ba6d69ea7f75228767ca7bbac7e6ce8e3102a
Danil <danil2003.2043@gmail.com>
2023-05-28 15:05:39 +0300

    deleted some shit

--- dg_face/detection.py
+++
@@ -1,7 +0,0 @@
-import dlib
-
-detector = dlib.get_frontal_face_detector()
-
-
-def detect_faces(img):
-    return detector(img, 1)
--- dg_face/utils.py
+++
@@ -1,5 +0,0 @@
-import cv2
-
-
-def load_image(path):
-    return cv2.cvtColor(cv2.imread(path), cv2.COLOR_BGR2RGB)
